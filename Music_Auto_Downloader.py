b4100918a5e02d7e3e75604cad3b4f4953eab9d8
Zachary 'Foxxin' Bukolt <foxxinthetechguy@proton.me>
2025-05-20 00:10:25 -0400

    Create basic version of the script.

---
+++ Music_Auto_Downloader.py
@@ -0,0 +1,24 @@
+import subprocess
+
+print("Music Auto Downloader v1.0.0\nPython Script by: Foxxin\nUses YT-DLP (https://github.com/yt-dlp/yt-dlp)")
+song_name = input("Please enter the name of the song you are looking for: ")
+song_artist = input("Please enter the artist of the song you are looking for: ")
+  
+search_query = f"ytsearch:{song_artist} {song_name}"
+
+command = [
+    "yt-dlp",
+    "-x",
+    "--audio-format", "mp3",
+    "--embed-thumbnail",
+    "--add-metadata",
+    "-o", "%(title)s.%(ext)s",
+    search_query
+]
+
+try:
+    subprocess.run(command, check=True)
+    print("Download completed successfully!")
+except subprocess.CalledProcessError as e:
+    print("Download failed!")
+    print(e)
---
+++ README.md
@@ -0,0 +1,1 @@
+This is my Music Auto Downloader. It uses a python script to craft the yt-dlp command for the user and downloads the music.
